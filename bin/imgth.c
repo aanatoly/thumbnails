09ab1f51d212b51346d0b5cc0db3812d5d8017b1
Anatoly <aanatoly@gmail.com>
2015-03-02 21:04:49 +0200

    fix for images with abs path

--- bin/imgth.c
+++ bin/imgth.c
@@ -1,10 +1,13 @@
 /* simple image thumbnailer */
 
 #include <unistd.h>
+#include <limits.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
 #include <glib.h>
 #include <gdk-pixbuf/gdk-pixbuf.h>
@@ -60,7 +63,7 @@ mk_thumb(const char *input, char *output, int size)
 {
     GdkPixbuf *pix;
     int rc;
-    char *cwd, *path, *uri;
+    char *path, *uri;
     struct stat statbuf;
     char mtime_str[100];
     GError *error = NULL;
@@ -69,12 +72,11 @@ mk_thumb(const char *input, char *output, int size)
     if (!pix)
         g_error("%s", error->message);
 
-    cwd = g_get_current_dir();
-    path = g_build_filename(cwd, input, NULL);
+    path = realpath(input, NULL);
     uri = g_filename_to_uri(path, NULL, NULL);
-    DBG("cwd %s\nname %s\npath %s\nuri %s\n", cwd, input, path, uri);
-    if (lstat(path, &statbuf)) 
-        g_error("lstat failed for %s\n", path);
+    DBG("name %s\npath %s\nuri %s\n", input, path, uri);
+    if (stat(path, &statbuf)) 
+        g_error("lstat failed for '%s': %s\n", path, strerror(errno));
 
     g_snprintf(mtime_str, 100, "%lu", statbuf.st_mtime);
     //gdk_pixbuf_set_option(pix, "tEXt::Thumb::MTime", mtime_str);
@@ -105,7 +107,6 @@ mk_thumb(const char *input, char *output, int size)
         
     if (slow_path) {
         g_object_unref(pix);
-        g_free(cwd);
         g_free(path);
         g_free(uri);
     }
